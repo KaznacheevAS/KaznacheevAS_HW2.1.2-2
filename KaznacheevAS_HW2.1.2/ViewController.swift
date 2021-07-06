e2f9477d4097bcc4c496d91acf9ce0e7595d227c
Kaznacheev <antkaznacheev@gmail.ru>
2021-07-06 23:22:43 +0300

    Светофор

--- KaznacheevAS_HW2.1.2/ViewController.swift
+++ KaznacheevAS_HW2.1.2/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var svetoforRed: UIView!
+    @IBOutlet weak var svetoforYelou: UIView!
+    @IBOutlet weak var svetoforGreen: UIView!
+    
+    @IBOutlet weak var bottomStartt: UIButton!
+    
+    
+    private var currenLing = CurrentLight.red
+    
+    private let alphaOf: CGFloat = 0.3
+    private let alphaOne: CGFloat = 1
+    
+    
+    private let radiusDate: CGFloat = 55
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        svetoforRed.layer.cornerRadius = radiusDate
+        svetoforGreen.layer.cornerRadius = radiusDate
+        svetoforYelou.layer.cornerRadius = radiusDate
+        
+        svetoforYelou.alpha = alphaOf
+        svetoforRed.alpha = alphaOf
+        svetoforGreen.alpha = alphaOf
+        
+        bottomStartt.layer.cornerRadius = 10
     }
 
+    @IBAction func activeBottomSart() {
+        
+        if bottomStartt.currentTitle == "START" {
+            bottomStartt.setTitle( "NEXT", for: .normal)
+        }
+        
+        
+        switch currenLing {
+            case .red:
+                svetoforGreen.alpha = alphaOf
+                svetoforRed.alpha = alphaOne
+                currenLing = .yellow
 
+            case .yellow:
+                svetoforRed.alpha = alphaOf
+                svetoforYelou.alpha = alphaOne
+                currenLing = .green
+            case .green:
+                svetoforYelou.alpha = alphaOf
+                svetoforGreen.alpha = alphaOne
+                currenLing = .red
+        }
+        
+        
+        
+        
+    }
+    
 }
 
